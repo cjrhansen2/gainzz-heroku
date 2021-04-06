b9db7817d1b83a444e3be10b20e80af5ae2fe4ad
cjrhansen2 <cjrhansen2@gmail.com>
2021-04-06 16:52:07 -0700

    image fix

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./style.css";
+import image from '../../../public/logo-wht.png'
 
 function Footer () {
 
     return (
         <footer id="sticky-footer" className=" py-4 bg-dark text-white-50">
             <div className="container text-center">
-            <small>Copyright &copy; <img src={`${process.env.PUBLIC_URL}/logo-wht.png`} width="75" height="25" alt="gainzz logo"/></small>
+            <small>Copyright &copy; <img src={image} width="75" height="25" alt="gainzz logo"/></small>
 
             </div>
         </footer>
